9d1971118e48d5c2a60e8d23d7424eda7ea95146
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-09-29 19:09:41 -0400

    Add dropdown list generation
    Dropdown list is now generated from the clients.xml file.
    Reads the file, creates option tags for each possible client, sorts the
    option tags alphabetically by client name, then appends it to the
    dropdown list.

--- partA/js/scripts.js
+++ partA/js/scripts.js
@@ -1,3 +1,34 @@
 $(() => {
-    console.log('Hello, world!');
+    $.get('../clients.xml', xmlData => {
+        /* GETs the clients.xml file, then parses the clients to polulate
+        the client selector dropdown list. Sort the options text values prior
+        to appending them to the dropdown list. */
+        const clients = xmlData.querySelectorAll('client');
+        const clientSelector = $('#clientSelection');
+        let optionTags = [];
+
+        clients.forEach(client => {
+            const optionTag = document.createElement('option');
+            optionTag.value = client.querySelector('id').innerHTML;
+            optionTag.text = $(client).find('clientName').text();
+
+            optionTags.push(optionTag);
+        });
+
+        optionTags.sort((tag1, tag2) => {
+            if(tag1.text.toLowerCase() < tag2.text.toLowerCase()) {
+                return -1;
+            }
+
+            if(tag1.text.toLowerCase() > tag2.text.toLowerCase()) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        optionTags.forEach(optionTag => {
+            clientSelector.append(optionTag);
+        });
+    });
 });
