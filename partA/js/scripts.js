6afbe580b55b78bc2a862fddc61187864a431fb4
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-09-30 16:39:52 -0400

    Add work record loading, sorting, iterating
    Work records now load based on the selected client. That work record
    changes whenever a new client is selected. Buttons are given to allow
    the user to iterate through all work records for the selected customer.
    These work records are sorted by work record number.

--- partA/js/scripts.js
+++ partA/js/scripts.js
@@ -1,4 +1,5 @@
 $(() => {
+    let index = 0;
     $.get('./clients.xml', xmlData => {
         /* GETs the clients.xml file, then parses the clients to polulate
         the client selector dropdown list. Sort the options text values prior
@@ -43,6 +44,8 @@ $(() => {
 
         $.get(invoiceFile, invoiceData => {
             const clientInvoiceElement = $('#clientInvoiceData');
+            const workRecords = invoiceData.querySelectorAll('workRecord');
+            let workRecArray = [];
             // XXX const clientInformation = $(invoiceData).find('client');
             // TODO: Parse through contact information without hardcoding the
             // tag names, prevents errors if the invoice.xsd file changes.
@@ -69,6 +72,80 @@ $(() => {
                 .text());
             clientInvoiceElement.append($(invoiceData).find('billRate')
                 .text());
+
+            workRecords.forEach(workRecord => {
+                workRecArray.push(workRecord);
+            });
+            workRecArray.sort((record1, record2) => {
+                // TODO: Reduce to only one return statement.
+                if(parseInt($(record1).attr('workNumber')) <
+                    $(record2).attr('workNumber')) {
+                    return -1;
+                }
+
+                if(parseInt($(record1).attr('workNumber')) >
+                    parseInt($(record2).attr('workNumber'))) {
+                    return 1;
+                }
+
+                return 0;
+            });
+
+            let workRecordDiv = document.createElement('div');
+            workRecordDiv.id = 'workRecords';
+            const workRecordButtonsDiv = document.createElement('div');
+            const nextButton = document.createElement('input');
+            const prevButton = document.createElement('input');
+
+            const displayWorkRecord = (workRecElmnt, workRecNum, workRecs) => {
+                const workRecord = workRecs[workRecNum];
+                workRecElmnt.text('');
+                workRecElmnt.append($(workRecord).find('workDescription')
+                    .text());
+            };
+
+            const checkButtonsForDisabling = (nextBtn, prevBtn, arraySize) => {
+                if (index === arraySize - 1) {
+                    nextBtn.prop('disabled', true);
+                } else {
+                    nextBtn.prop('disabled', false);
+                }
+
+                if (index === 0) {
+                    prevBtn.prop('disabled', true);
+                } else {
+                    prevBtn.prop('disabled', false);
+                }
+            };
+
+            prevButton.id = 'btnPrev';
+            prevButton.value = 'Previous';
+            prevButton.type = 'button';
+            $(prevButton).prop('disabled', true);
+            workRecordButtonsDiv.append(prevButton);
+
+            prevButton.addEventListener('click', () => {
+                displayWorkRecord(workRecordDiv, --index, workRecArray);
+                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
+                    workRecArray.length);
+            });
+
+            nextButton.id = 'btnNext';
+            nextButton.value = 'Next';
+            nextButton.type = 'button';
+            workRecordButtonsDiv.append(nextButton);
+
+            nextButton.addEventListener('click', () => {
+                displayWorkRecord(workRecordDiv, ++index, workRecArray);
+                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
+                    workRecArray.length);
+            });
+
+            clientInvoiceElement.append(workRecordDiv);
+            clientInvoiceElement.append(workRecordButtonsDiv);
+
+            workRecordDiv = $('#workRecords');
+            displayWorkRecord(workRecordDiv, 0, workRecArray);
 
         });
     });
