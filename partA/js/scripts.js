4baa38708115ff5320703e89e6fcddef2930d347
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-09-30 21:12:50 -0400

    Code cleanup, rearrangements, comments
    I should have split this into many commits. Oh well.
    Cleaned the source code, use more jQuery functions and selectors over
    HTML5, remove unused lines of code, rearranged line of code, added
    comments for functions.

--- partA/js/scripts.js
+++ partA/js/scripts.js
@@ -2,38 +2,43 @@ $(() => {
     let index = 0;
     $.get('./clients.xml', xmlData => {
         /* GETs the clients.xml file, then parses the clients to polulate
-        the client selector dropdown list. Sort the options text values prior
-        to appending them to the dropdown list. */
+            the client selector dropdown list. Sort the options text values
+            prior to appending them to the dropdown list. */
         const clients = xmlData.querySelectorAll('client');
-        const clientSelector = $('#clientSelection');
         let optionTags = [];
 
         clients.forEach(client => {
+            /* Create an option tag for each client, add it to an array of
+                client tags */
             const optionTag = document.createElement('option');
-            optionTag.value = client.querySelector('id').innerHTML;
+            optionTag.value = $(client).find('id').text();
             optionTag.text = $(client).find('clientName').text();
             optionTags.push(optionTag);
-        });
+        });// clients.forEach(...)
 
         optionTags.sort((tag1, tag2) => {
-            // TODO: Reduce to only one return statement.
+            /* Sort the elements of optionTags by option name (client name). */
+            let exit = 0;
+
             if(tag1.text.toLowerCase() < tag2.text.toLowerCase()) {
-                return -1;
+                exit = -1;
             }
 
             if(tag1.text.toLowerCase() > tag2.text.toLowerCase()) {
-                return 1;
+                exit = 1;
             }
 
-            return 0;
-        });
+            return exit;
+        });// optionTags.sort(...)
 
         optionTags.forEach(optionTag => {
-            clientSelector.append(optionTag);
-        });
-    });
+            $('#clientSelection').append(optionTag);
+        });// optionTags.forEach(...);
+    });// $.get('clients.xml, ...)
 
     $('#clientSelection').change(() => {
+        /* On client selection, grabs the clients information, work record
+            information, sets up the various inputs and their event listeners.*/
         const selectedValue = $('#clientSelection').val();
         const invoiceFile =
             (parseInt(selectedValue) === 1)
@@ -41,143 +46,155 @@ $(() => {
             // usual pattern for clients with the ID 1.
                 ? './invoice.xml'
                 : `./invoice${selectedValue}.xml`;
+        index = 0;
 
         $.get(invoiceFile, invoiceData => {
-            const clientInvoiceElement = $('#clientInvoiceData');
             const workRecords = invoiceData.querySelectorAll('workRecord');
             let workRecArray = [];
-            // XXX const clientInformation = $(invoiceData).find('client');
-            // TODO: Parse through contact information without hardcoding the
-            // tag names, prevents errors if the invoice.xsd file changes.
-            // TODO: Currently just dumps text to screen, must format data.
+            const getInvoiceValue = elementName => {
+                return $(invoiceData).find(elementName).text();
+            };
+            // TODO: Future thought:parsing through contact information without
+            // hardcoding the tag names, prevents errors if the invoice.xsd
+            // file changes. Good feature if I have the time.
 
-            clientInvoiceElement.text('');
-            clientInvoiceElement.append($(invoiceData).find('contactFirstName')
-                .text() + ' ');
-            clientInvoiceElement.append($(invoiceData).find('contactLastName')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('clientStreet')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('clientCity')
-                .text() + ', ');
-            clientInvoiceElement.append($(invoiceData).find('clientProvince')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('clientPostalCode')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('clientDiscount')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('info')
-                .attr('invoiceNumber'));
-            clientInvoiceElement.append($(invoiceData).find('invoiceDate')
-                .text());
-            clientInvoiceElement.append($(invoiceData).find('billRate')
-                .text());
+            // Display Client, and Contact information.
+            $('#contactName').text(`Contact:
+                ${getInvoiceValue('contactFirstName')}
+                ${getInvoiceValue('contactLastName')}`);
+            $('#clientStreet').text(getInvoiceValue('clientStreet'));
+            $('#clientCity').text(`${getInvoiceValue('clientCity')},
+                ${getInvoiceValue('clientProvince')}`);
+            $('#clientPostalCode').text(getInvoiceValue('clientPostalCode'));
+            $('#clientDiscount').text(`Discount:
+                ${getInvoiceValue('clientDiscount') * 100}%`);
+
+            // Display invoice information.
+            $('#invoiceNumber').text(`Invoice:
+                ${$(invoiceData).find('info').attr('invoiceNumber')}`);
+            $('#invoiceDate').text(`Date: ${getInvoiceValue('invoiceDate')}`);
+            $('#billRate').text(`Bill Rate: ${getInvoiceValue('billRate')}`);
 
             workRecords.forEach(workRecord => {
                 workRecArray.push(workRecord);
-            });
+            });// workRecords.forEach(...)
+
             workRecArray.sort((record1, record2) => {
-                // TODO: Reduce to only one return statement.
+                let exit = 0;
                 if(parseInt($(record1).attr('workNumber')) <
                     $(record2).attr('workNumber')) {
-                    return -1;
+                    exit = -1;
                 }
 
                 if(parseInt($(record1).attr('workNumber')) >
                     parseInt($(record2).attr('workNumber'))) {
-                    return 1;
+                    exit = 1;
                 }
 
-                return 0;
+                return exit;
             });
 
-            let workRecordDiv = document.createElement('div');
-            workRecordDiv.id = 'workRecords';
-            const workRecordButtonsDiv = document.createElement('div');
+            // Define the "Next" button, add to DOM
             const nextButton = document.createElement('input');
+            nextButton.id = 'btnNext';
+            nextButton.value = 'Next';
+            nextButton.type = 'button';
+            $('#nextBtnLocation').html(nextButton);
+
+            // Define the "Previous" button, add to DOM
             const prevButton = document.createElement('input');
-            const searchBox = document.createElement('input');
-            const searchButton = document.createElement('input');
-
-            const displayWorkRecord = (workRecElmnt, workRecNum, workRecs) => {
-                const workRecord = workRecs[workRecNum];
-                workRecElmnt.text('');
-                workRecElmnt.append($(workRecord).find('workDescription')
-                    .text());
-            };
-
-            const checkButtonsForDisabling = (nextBtn, prevBtn, arraySize) => {
-                if (index === arraySize - 1) {
-                    nextBtn.prop('disabled', true);
-                } else {
-                    nextBtn.prop('disabled', false);
-                }
-
-                if (index === 0) {
-                    prevBtn.prop('disabled', true);
-                } else {
-                    prevBtn.prop('disabled', false);
-                }
-            };
-
             prevButton.id = 'btnPrev';
             prevButton.value = 'Previous';
             prevButton.type = 'button';
             $(prevButton).prop('disabled', true);
-            workRecordButtonsDiv.append(prevButton);
+            $('#previousBtnLocation').html(prevButton);
 
-            prevButton.addEventListener('click', () => {
-                displayWorkRecord(workRecordDiv, --index, workRecArray);
-                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
-                    workRecArray.length);
-            });
-
-            nextButton.id = 'btnNext';
-            nextButton.value = 'Next';
-            nextButton.type = 'button';
-            workRecordButtonsDiv.append(nextButton);
-
-            nextButton.addEventListener('click', () => {
-                displayWorkRecord(workRecordDiv, ++index, workRecArray);
-                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
-                    workRecArray.length);
-            });
-
-            searchBox.id = 'searchBox';
+            // Define the "Seach" textfield, add to DOM
+            const searchBox = document.createElement('input');
+            searchBox.id = 'txtSearch';
             searchBox.type = 'text';
+            $('#txtSearchLocation').html(searchBox);
+
+            // Define the "Seach" button, add to DOM
+            const searchButton = document.createElement('input');
             searchButton.id = 'btnSearch';
             searchButton.type = 'button';
             searchButton.value = 'Search';
-            $('#searchBarLocation').append(searchBox);
-            $('#searchBarLocation').append(searchButton);
+            $('#btnSearchLocation').html(searchButton);
+
+            const displayWorkRecord = (workRecNum, workRecs) => {
+                /* Display work record information. */
+                const workRecord = workRecs[workRecNum];
+
+                const getWorkRecordValue = element => {
+                    return $(workRecord).find(element).text();
+                };
+
+                $('#workNumber').text($(workRecord).attr('workNumber'));
+                $('#workDescription')
+                    .text(getWorkRecordValue('workDescription'));
+                $('#workDate').text(getWorkRecordValue('workDate'));
+                $('#workTypeNumber').text(getWorkRecordValue('workTypeNumber'));
+                $('#billedHours').text(getWorkRecordValue('billedHours'));
+            };// displayWorkRecord(...)
+
+            const checkButtonsForDisabling = (nextBtn, prevBtn, arraySize) => {
+                /* Checks which work record index the user is currently at, and
+                    disables the buttons when it would cause the user to
+                    overflow. */
+                (index === arraySize - 1)
+                    ? nextBtn.prop('disabled', true)
+                    : nextBtn.prop('disabled', false);
+
+                (index === 0)
+                    ? prevBtn.prop('disabled', true)
+                    : prevBtn.prop('disabled', false);
+            };// checkButtonsForDisabling(...)
 
             const displayErrorMessage = message => {
-                workRecordDiv.text(message);
-            };
+                /* I'll give you one guess what this function does. */
+                $('#errorMessages').text(message);
+            };// displayErrorMessage(...)
 
-            searchButton.addEventListener('click', () => {
-                const userInput = $('#searchBox').val();
+            $('#btnPrev').click(() => {
+                /* Handle clicking on the "Previous" button. */
+                displayWorkRecord(--index, workRecArray);
+                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
+                    workRecArray.length);
+            });// $('btnPrev').click(...)
+
+            $('#btnNext').click(() => {
+                /* Handle clicking on the "Previous" button. */
+                displayWorkRecord(++index, workRecArray);
+                checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
+                    workRecArray.length);
+            });// $('#btnNext').click(...)
+
+            $('#btnSearch').click(() => {
+                /* Checks users input to the seach box, and jumps to that work
+                    record if it exists. */
+                const userInput = $('#txtSearch').val();
+
                 if(!$.isNumeric(userInput)) {
+                    // Fails because input contains non-numbers.
                     displayErrorMessage('Search term must be a number.');
                 } else {
                     if(parseInt(userInput) >= workRecArray.length) {
+                        // Fails because search is greater than array.
+                        // TODO: Just realized this is *NOT* what the search bar
+                        // was suppose to do. It should jump to the matching
+                        // work number, not the matching index number.
                         displayErrorMessage('Work Record does not exist.');
                     } else {
                         index = userInput;
-                        displayWorkRecord(workRecordDiv, index,
-                            workRecArray);
+                        displayWorkRecord(index, workRecArray);
                         checkButtonsForDisabling($('#btnNext'), $('#btnPrev'),
                             workRecArray.length);
                     }
                 }
-            });
-
-            clientInvoiceElement.append(workRecordDiv);
-            clientInvoiceElement.append(workRecordButtonsDiv);
-
-            workRecordDiv = $('#workRecords');
-            displayWorkRecord(workRecordDiv, 0, workRecArray);
-
-        });
-    });
-});
+            });// $('#btnSearch').click(...)
+            // Display the first work record on client selection.
+            displayWorkRecord(0, workRecArray);
+        });// $.get(invoiceFile, ...)
+    });// $('#clientSelection').change(...)
+});// ()
