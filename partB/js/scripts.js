47902376738e44ff062c18f2ecddbffd600f9686
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-10-01 00:26:59 -0400

    Add AJAX `getUsers.php` call
    Application calls `getUsers.php` asynchronously, and handles the XML file
    sent back in the response. Currently does not display this information,
    instead just dumps to console to verify it exists.

--- partB/js/scripts.js
+++ partB/js/scripts.js
@@ -1,3 +1,21 @@
 (() => {
-    console.log('Hello, world!');
+    const getXmlHttpRequest = () => { return new XMLHttpRequest(); }
+
+    const getListOfUsers = () => {
+        const PHP_ADDRESS = 'http://localhost:9000/getUsers.php';
+        try {
+            const xhr = getXmlHttpRequest();
+            xhr.open('GET', PHP_ADDRESS);
+            xhr.send();
+            xhr.addEventListener('readystatechange', () => {
+                if(xhr.readyState === 4) {
+                    console.log(xhr.responseXML);
+                }
+            });
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    getListOfUsers();
 })();
